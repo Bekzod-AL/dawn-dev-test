7f4cd9f84a8b14f380143595ff20179750269145
Bekzod-AL <bekzod@appliedlabs.io>
2024-08-14 16:15:49 +0500

    feat: add pause on out of view & remove nesting

--- assets/lazy-loading.js
+++ assets/lazy-loading.js
@@ -1,50 +1,75 @@
 // web component
 
-const videoElement = document.createElement('template');
-videoElement.innerHTML = `
-  <video data-lazy-load autoplay loop  muted>
-    <source>
-  </video>
-`;
-
 class LazyVideoComponent extends HTMLElement {
   constructor() {
     super();
+
+    this.videoElement = document.createElement('template');
+    this.videoElement.innerHTML = `
+      <video data-lazy-load autoplay loop muted>
+        <source>
+      </video>
+    `;
+    this.videoAttribute = 'data-lazy-load';
+
     if (!this.shadowRoot) {
       this.attachShadow({ mode: 'open' });
-      this.shadowRoot.append(videoElement.content.cloneNode(true));
+      this.shadowRoot.append(this.videoElement.content.cloneNode(true));
     }
     this.observer = null;
     this.update();
   }
 
   connectedCallback() {
+    this.setObserver();
+  }
+
+  disconnectedCallback() {
+    this.observer?.disconnect();
+  }
+
+  setObserver() {
     this.observer = new IntersectionObserver((entries) => {
+      const video = this.shadowRoot.querySelector('video');
+
       entries.forEach((entry) => {
-        if (!entry.isIntersecting) return;
-
-        const video = this.shadowRoot.querySelector('video');
-
-        Array.from(video.children).forEach((child) => {
-          if (child.tagName === 'SOURCE') {
-            child.src = child.getAttribute('data-src');
-          }
-        });
-
-        video.load();
-        video.removeAttribute('data-lazy-load');
-
-        this.observer.unobserve(video);
+        if (entry.isIntersecting) {
+          this.handleVideoInView(video);
+        } else {
+          this.handleVideoOutOfView(video);
+        }
       });
     });
 
     this.observer.observe(this);
   }
 
-  disconnectedCallback() {
-    if (this.observer) {
-      this.observer.disconnect();
+  handleVideoInView(video) {
+    if (video.hasAttribute(this.videoAttribute)) {
+      this.loadVideo(video);
+      this.observer.unobserve(video);
+    } else {
+      console.log(`Resuming video at ${video.currentTime.toFixed(2)}s / ${video.duration.toFixed(2)}s`);
+      video.play();
     }
+  }
+
+  handleVideoOutOfView(video) {
+    if (!video.hasAttribute(this.videoAttribute)) {
+      console.log(`Video paused at ${video.currentTime.toFixed(2)}s / ${video.duration.toFixed(2)}s`);
+      video.pause();
+    }
+  }
+
+  loadVideo(video) {
+    Array.from(video.children).forEach((child) => {
+      if (child.tagName === 'source'.toUpperCase()) {
+        child.src = child.getAttribute('data-src');
+      }
+    });
+
+    video.load();
+    video.removeAttribute(this.videoAttribute);
   }
 
   update() {
