034ce6e2eb1a6d50badc17970a2767ada531baa6
Bekzod-AL <bekzod@appliedlabs.io>
2024-09-18 19:50:18 +0500

    feat: add comments

--- assets/custom-predictive-search.js
+++ assets/custom-predictive-search.js
@@ -1,3 +1,33 @@
+{
+  /*
+    This is predictive search component that allows you to get specific data based on attributes you pass.
+    You can refer to this link to check the available parameters: https://shopify.dev/docs/api/ajax/reference/predictive-search
+    
+    You can also set a "timer" attribute, which controls the debounce time before fetching the data, by default it is 400.
+
+    To use this component, you only need to include the tag, an input field, and a render element with 
+    the required 'data-predictive-results' attribute.
+
+    The component also requires the 'content-id' attribute with a value from your render-search-results.liquid file.
+
+    Example usage:
+
+    <prediction-search
+      content-id="data-search-content"
+      timer="400"
+      resources[type]="products, queries"
+      resources[limit]="10"
+      resources[options][unavailable_products]="hide"
+    >
+      <input
+        type="search"
+        value="{{ search.terms |  escape }}"
+      >
+      <div class="search-results" data-predictive-results></div>
+    </prediction-search> 
+  */
+}
+
 class PredSearch extends HTMLElement {
   constructor() {
     super();
