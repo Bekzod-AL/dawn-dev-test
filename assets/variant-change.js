1464a44f5d46991c5da46492f30398a43fff5d5f
Bekzod-AL <bekzod@appliedlabs.io>
2024-10-10 17:09:21 +0500

    feat: add variant select

--- assets/variant-change.js
+++ assets/variant-change.js
@@ -1,63 +1,87 @@
 class VariantChange extends HTMLElement {
-  // change structure into getting active from url ?variant , to set active variants and make it dyncamic
   constructor() {
     super();
     this.selectedOptions = null;
     this.currentVariant = null;
-    this.variantTrigger = 'button';
+    this.variantTrigger = 'data-variant-trigger';
     this.variantValueAttribute = 'data-variant-value';
     this.variantActiveAttribute = 'data-active-variant';
     this.variantIdAttribute = 'data-variant-id';
     this.groupIdAttribute = 'data-group-id';
+    this.formInputAttribute = 'data-form-input';
+    this.submitButtonAttribute = 'data-submit-button';
 
-    this.addEventListener('click', this.handleClick);
+    this.setVariantFromUrl();
+    this.addEventListener('click', this.handleInputClick);
+    this.addEventListener('change', this.handleSelectChange);
   }
 
-  handleClick(event) {
+  handleInputClick(event) {
     const target = event.target;
 
-    if (target.tagName.toLowerCase() === this.variantTrigger && target.hasAttribute(this.variantValueAttribute)) {
-      this.changeVariant(target);
+    if (target.hasAttribute(this.variantTrigger)) {
+      this.changeInputVariantToActive(target);
 
       this.selectedOptions = this.getSelectedOptions();
       this.currentVariant = this.getCurrentVariant();
 
-      this.changeProductFormInputData('product-form-input', this.currentVariant.id);
-
-      console.log(this.currentVariant);
-      this.fetchProduct();
+      this.urlReplace(this.currentVariant.id);
+      this.changeProductFormInputData(this.currentVariant.id);
+      this.updateProductPage();
     }
   }
 
-  changeVariant(target) {
+  handleSelectChange(event) {
+    const target = event.target;
+
+    if (target.tagName.toLowerCase() == 'select') {
+      this.selectedOptions = Array.from(this.querySelectorAll('select'), (select) => select.value);
+      this.currentVariant = this.getCurrentVariant();
+
+      this.urlReplace(this.currentVariant.id);
+
+      this.changeProductFormInputData(this.currentVariant.id);
+      this.updateProductPage();
+    }
+  }
+
+  setVariantFromUrl() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const variantId = urlParams.get('variant');
+
+    if (variantId) {
+      this.changeProductFormInputData(variantId);
+    }
+  }
+
+  urlReplace(value) {
+    const currentUrlAttribute = 'data-current-url';
+    const currentUrl = this.querySelector(`[${currentUrlAttribute}]`).getAttribute(`${currentUrlAttribute}`);
+
+    window.history.replaceState({}, '', `${currentUrl}?variant=${value}`);
+  }
+
+  changeInputVariantToActive(target) {
     const variantId = target.getAttribute(this.variantIdAttribute);
     const variantGroup = this.querySelector(`[${this.groupIdAttribute}="${variantId}"]`);
 
-    variantGroup.querySelectorAll(`button[${this.variantActiveAttribute}]`).forEach((variant) => {
+    variantGroup.querySelectorAll(`[${this.variantTrigger}]`).forEach((variant) => {
       variant.removeAttribute(this.variantActiveAttribute);
     });
 
     target.setAttribute(this.variantActiveAttribute, '');
   }
 
-  changeProductFormInputData(inputId, variantId) {
-    const input = this.querySelector(`input[id=${inputId}]`);
+  changeProductFormInputData(variantId) {
+    const input = this.querySelector(`input[${this.formInputAttribute}]`);
 
     input.value = variantId;
   }
 
   getSelectedOptions() {
-    const input = this.querySelector(`input[id="product-form-input"]`);
-    const variants = JSON.parse(this.querySelector('[type="application/json"]').innerHTML);
-
-    const test = variants.find((item) => {
-      return item.id == input.value;
-    });
-
-    return test.options;
-    // return Array.from(this.querySelectorAll(`button[${this.variantActiveAttribute}]`)).map((option) =>
-    //   option.getAttribute(this.variantValueAttribute)
-    // );
+    return Array.from(this.querySelectorAll(`[${this.variantActiveAttribute}]`)).map((option) =>
+      option.getAttribute(this.variantValueAttribute)
+    );
   }
 
   getCurrentVariant() {
@@ -68,12 +92,28 @@ class VariantChange extends HTMLElement {
     });
   }
 
-  fetchProduct() {
-    fetch(`t-shirt?variant=${this.currentVariant.id}?section_id=product-section`)
-      .then((response) => response.text())
-      .then((product) => {
-        document.querySelector('.product-section').innerHTML = product;
-      });
+  async updateProductPage() {
+    const submitButton = document.querySelector(`[${this.submitButtonAttribute}]`);
+    submitButton.disabled = true;
+
+    try {
+      const url = window.location.href;
+
+      const res = await fetch(url);
+
+      if (!res.ok) {
+        throw new Error(`Error: ${res.status} ${res.statusText}`);
+      }
+
+      const responseData = await res.text();
+      const resultsMarkup = new DOMParser().parseFromString(responseData, 'text/html');
+
+      document.querySelector('.product-section').innerHTML = resultsMarkup.querySelector('.product-section').innerHTML;
+    } catch (error) {
+      console.error('Error: ', error);
+    } finally {
+      submitButton.disabled = false;
+    }
   }
 }
 
